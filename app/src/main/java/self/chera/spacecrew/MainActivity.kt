656e9beb5ce6a1b788c0b8f003afa4142d42a183
keychera <kyogatama@gmail.com>
2024-05-02 15:32:17 +0900

    turn on bluetooth

--- app/src/main/java/self/chera/spacecrew/MainActivity.kt
+++ app/src/main/java/self/chera/spacecrew/MainActivity.kt
@@ -1,10 +1,18 @@
+@file:OptIn(ExperimentalPermissionsApi::class)
+
 package self.chera.spacecrew
 
 import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -16,11 +24,16 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.MultiplePermissionsState
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import self.chera.spacecrew.ui.theme.SpacecrewTheme
 
@@ -42,42 +55,70 @@ class MainActivity : ComponentActivity() {
 }
 
 @RequiresApi(Build.VERSION_CODES.S)
-@OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun FeatureThatUseBluetooth() {
+fun ComponentActivity.FeatureThatUseBluetooth() {
     val bluetoothPermissionState = rememberMultiplePermissionsState(
-        listOf(Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_SCAN),
+        listOf(
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_SCAN,
+            Manifest.permission.BLUETOOTH_CONNECT
+        ),
     )
 
     if (bluetoothPermissionState.allPermissionsGranted) {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceAround,
-                modifier = Modifier
-                    .height(150.dp)
-                    .fillMaxWidth()
-            ) {
-                Text(text = "Bluetooth is allowed")
-                Text(text = "Bluetooth is OFF")
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "Turn ON\nBluetooth")
-                }
-            }
-        }
+        BluetoothMainScreen()
     } else {
+        Grant(bluetoothPermissionState)
+    }
+}
+
+@Composable
+fun ComponentActivity.BluetoothMainScreen() {
+    val manager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+    val bluetooth = manager.adapter
+
+    var isON by remember { mutableStateOf(bluetooth.isEnabled) }
+
+    val enableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+    val toggleBT = rememberLauncherForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { isON = bluetooth.isEnabled }
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxSize()
+            verticalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier
+                .height(150.dp)
+                .fillMaxWidth()
         ) {
-            Text("Bluetooth is needed")
-            Button(onClick = { bluetoothPermissionState.launchMultiplePermissionRequest() }) {
-                Text("Grant permission")
+            Text(text = "Bluetooth is allowed")
+            Text(text = "Bluetooth is ${if (isON) "ON" else "OFF"}")
+            Button(
+                enabled = !bluetooth.isEnabled,
+                onClick = { toggleBT.launch(enableIntent) }
+            ) {
+                Text(text = "Turn ON\nBluetooth")
             }
+        }
+    }
+}
+
+@Composable
+fun Grant(
+    permissionStates: MultiplePermissionsState
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text("Bluetooth is needed")
+        Button(onClick = { permissionStates.launchMultiplePermissionRequest() }) {
+            Text("Grant permission")
         }
     }
 }
@@ -87,6 +128,6 @@ fun FeatureThatUseBluetooth() {
 @Composable
 fun GreetingPreview() {
     SpacecrewTheme {
-        FeatureThatUseBluetooth()
+
     }
 }
