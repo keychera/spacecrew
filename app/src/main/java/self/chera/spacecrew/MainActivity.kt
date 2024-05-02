25c4ca703f75b2d730c1b86c54ecea9b617c337d
keychera <kyogatama@gmail.com>
2024-05-02 14:15:22 +0900

    permission handling with Accompanist

--- app/src/main/java/self/chera/spacecrew/MainActivity.kt
+++ app/src/main/java/self/chera/spacecrew/MainActivity.kt
@@ -1,11 +1,16 @@
 package self.chera.spacecrew
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -14,9 +19,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import self.chera.spacecrew.ui.theme.SpacecrewTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -25,39 +34,59 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    FeatureThatUseBluetooth()
                 }
             }
         }
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.S)
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun FeatureThatUseBluetooth() {
+    val bluetoothPermissionState = rememberMultiplePermissionsState(
+        listOf(Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_SCAN),
     )
-}
 
-@Composable
-fun GrantScreen() {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Text(text = "Allow bluetooth")
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Grant permission")
+    if (bluetoothPermissionState.allPermissionsGranted) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceAround,
+                modifier = Modifier
+                    .height(150.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(text = "Bluetooth is allowed")
+                Text(text = "Bluetooth is OFF")
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Turn ON\nBluetooth")
+                }
+            }
+        }
+    } else {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Text("Bluetooth is needed")
+            Button(onClick = { bluetoothPermissionState.launchMultiplePermissionRequest() }) {
+                Text("Grant permission")
+            }
         }
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.S)
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SpacecrewTheme {
-        GrantScreen()
+        FeatureThatUseBluetooth()
     }
 }
